2bfef42f70603852b2a6aa4a4fa0814b876f4e75
ydjabela <ydjabela@blendbow.com>
2022-07-15 19:16:35 +0200

    Revert "creat request users and projects"
    This reverts commit 3e48916a8c2dc1de0afa2d86d23ecf8f5c3ff4b8.

--- SoftDesk/authentification/views.py
+++ SoftDesk/authentification/views.py
@@ -1,16 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-from authentification.models import User
-from authentification.serializers import UserSerializer
-
-
-class UsersAPIView(APIView):
-
-    def get(self, *args, **kwargs):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
