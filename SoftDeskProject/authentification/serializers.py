f8dfcf3e22ef5a37d615a150baf670dc6374f35c
ydjabela <ydjabela@blendbow.com>
2022-07-27 17:42:04 +0200

    update paswword

--- SoftDeskProject/authentification/serializers.py
+++ SoftDeskProject/authentification/serializers.py
@@ -3,20 +3,16 @@ from rest_framework import serializers
 from .models import User
 from django.contrib.auth.hashers import make_password
 
+
 class UserSerializer(serializers.ModelSerializer):
 
     date_joined = serializers.ReadOnlyField()
+
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
 
     class Meta(object):
         model = User
         fields = ('id', 'email', 'first_name', 'last_name',
                   'date_joined', 'password')
         extra_kwargs = {'password': {'write_only': True}}
-
-        def validate_password(self, value: str) -> str:
-            """
-            Hash value passed by user.
-            :param value: password of a user
-            :return: a hashed version of the password
-            """
-            return make_password(value)
