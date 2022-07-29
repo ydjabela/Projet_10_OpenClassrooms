b43f211ce0a153dd74cc7e6656ae07fbd9e52ee5
ydjabela <ydjabela@blendbow.com>
2022-07-29 17:51:27 +0200

    update project and creat project

--- SoftDeskProject/SoftDeskProject/urls.py
+++ SoftDeskProject/SoftDeskProject/urls.py
@@ -13,19 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from authentification.views import CreateUserAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from project.views import ProjectsAPIView
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from project.views import projectViewset
 
+# Ici nous créons notre routeur
+router = routers.SimpleRouter()
+# Puis lui déclarons une url basée sur le mot clé ‘category’ et notre view
+# afin que l’url générée soit celle que nous souhaitons ‘/api/category/’
+router.register('project', projectViewset, basename='project')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # path('user/', UserAPIView.authenticate_user()),
     path('create/', CreateUserAPIView.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/projects/', ProjectsAPIView.as_view()),
+    path('api/', include(router.urls))
+    # Il faut bien penser à ajouter les urls du router dans la liste des urls disponibles.
 ]
+
--- SoftDeskProject/project/views.py
+++ SoftDeskProject/project/views.py
@@ -1,23 +1,13 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from project.models import Project, Contributor, Issue, Comment
 from project.serializers import ProjectSerializer, ContributorSerializer, IssueSerializer, CommentSerializer
-from project.permissions import IsAdminAuthenticated, IsAuthor, IsProjectAuthor
+from project.permissions import IsAdminAuthenticated, IsAuthor, IsProjectAuthor, IsContributor
 
 
-class ProjectsAPIView(APIView):
-    permission_classes = [IsAdminAuthenticated, IsProjectAuthor, IsAuthor]
-
-    def get(self, *args, **kwargs):
-        projects = Project.objects.all()
-        serializer = ProjectSerializer(projects, many=True)
-        return Response(serializer.data)
+class projectViewset(ModelViewSet):
+    serializer_class = ProjectSerializer
+    #permission_classes = [IsContributor, IsProjectAuthor]
 
     def get_queryset(self):
-        queryset = Project.objects.filter(contributors__id=self.request.user.id)
-        return queryset
-
-    def perform_create(self, serializer_class):
-        project = serializer_class.save()
-        contributor = Contributor.objects.create(project=project, role="AUTHOR", user=self.request.user)
+        print(self.request.user.id)
+        return Project.objects.filter(contributor__id=self.request.user.id)
